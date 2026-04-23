2d4087b418e2f4a3f1ffabfbe126488aeace1ae0
ARP <andrewarp06@gmail.com>
2026-04-23 21:30:10 +0200

    Cambio del EdgeNode 21/04
    Added a delay before processing the data in enviarDato method.

--- src/main/java/Edgenode.java
+++ src/main/java/Edgenode.java
@@ -11,9 +11,15 @@ public class EdgeNode {
         String sensorId = "F" + fogNode.getIdFog() + "-E" + idEdge;
         double temperatura = 15 + Math.random() * 25;
         temperatura = Math.round(temperatura * 10.0) / 10.0;
-
         SensorData dato = new SensorData(sensorId, temperatura);
+
         System.out.println("[EDGE " + idEdge + " de FOG " + fogNode.getIdFog() + "] Dato generado: " + dato);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         fogNode.procesarDato(dato, idEdge);
     }
 }
